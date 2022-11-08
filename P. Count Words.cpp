6322c16f6b4bc8d54e02d65bbd381ca1347f9e7a
megamind1230 <90935539+megamind1230@users.noreply.github.com>
2022-11-08 18:18:31 +0200

    Update P. Count Words.cpp

--- P. Count Words.cpp
+++ P. Count Words.cpp
@@ -6,7 +6,7 @@ using namespace std;
 #define ln <<'\n';
 int main()
 {
-
+https://codeforces.com/group/MWSDmqGsZm/contest/219856/problem/P
 #ifndef ONLINE_JUDGE
     freopen("input.txt",  "r",  stdin);
     freopen("output.txt", "w", stdout);
@@ -20,6 +20,7 @@ int main()
         replace( s.begin(), s.end(), wantedToBeDeleted[i], '0'); // replace all 'x' to 'y'
         s.erase(remove(s.begin(), s.end(), '0'), s.end());
     }
+    //-----------------
     
     int words = 0;
     for (int i = 0; i < s.size() -1 ; ++i)
@@ -30,3 +31,36 @@ int main()
     cout<<words+1;
 }
 */
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt",  "r",  stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
+
+    string TheWholeLine; getline(cin, TheWholeLine);
+    string wantedToBeDeleted = "!.,?";
+
+    for(int i = 0; i < 5; ++i){
+        replace( TheWholeLine.begin(), TheWholeLine.end(), wantedToBeDeleted[i], ' '); // replace unwanted with a {space}
+        //TheWholeLine.erase(remove(TheWholeLine.begin(), TheWholeLine.end(), '0'), TheWholeLine.end()); // no erasing necessary
+    }
+    int new_size = TheWholeLine.size() + 1;//increase the size by 1... to displace the \0 for a one more char to the right
+    TheWholeLine.resize(new_size);
+    TheWholeLine[new_size-1] = ' '; // and put a {space} at the end { this means that the last char can be changed to a space with no errors }
+
+    int words = 0;
+    for(int i = 0; i < TheWholeLine.size() ; ++i) //(new_size-1) =  TheWholeLine.size()
+    {
+        if(TheWholeLine[i] != ' ' && TheWholeLine[i+1] == ' ')// {like this} >> lik{e}{ }this // 
+        {
+            words++;
+        }
+    }
+    cout<<words;
+}
